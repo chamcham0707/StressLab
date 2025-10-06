1c6d4504fbdb9d15ce1836bb11bcbd34cc3858ef
chamcham0707 <cheamin6306@naver.com>
2025-10-06 16:44:38 +0900

    init

---
+++ src/test/java/org/chamcham/stresslab/StressLabApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chamcham.stresslab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StressLabApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
